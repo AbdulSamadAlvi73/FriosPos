83773f2bb709a75cf7352441b602441e9f8c08af
Abdul Samad 71 <116957635+AbdulSamadAlvi73@users.noreply.github.com>
2025-04-27 16:24:20 +0500

    status

--- resources/views/corporate_admin/view_orders/index.blade.php
+++ resources/views/corporate_admin/view_orders/index.blade.php
@@ -124,7 +124,7 @@
                                             </select>
                                         </td>
 
-                                        <td>{{ $order->status }}</td>
+                                        {{-- <td>{{ $order->status }}</td> --}}
                                         <td>${{ number_format($order->total_amount, 2) }}</td>
                                         <td>{{ $order->date_transaction->format('M d, Y h:i A') }}</td>
                                     </tr>
